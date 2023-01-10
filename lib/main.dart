db32df6415e604d88ac25675a9fea33e620c3db6
PieroAlvaradoP <79418881+PieroAlvaradoP@users.noreply.github.com>
2023-01-10 16:31:06 -0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,239 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'graph_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'GastoWallet',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int currentPage = 9;
+  late PageController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = PageController(
+      initialPage: currentPage,
+      viewportFraction: 0.4,
+    );
+  }
+
+  Widget _bottomAction(IconData icon) {
+    return InkWell(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Icon(icon),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        notchMargin: 8.0,
+        shape: const CircularNotchedRectangle(),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: <Widget>[
+            _bottomAction(FontAwesomeIcons.clockRotateLeft),
+            _bottomAction(FontAwesomeIcons.chartPie),
+            const SizedBox(width: 48.0),
+            _bottomAction(FontAwesomeIcons.wallet),
+            _bottomAction(Icons.settings),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: _body(),
+    );
+  }
+
+  Widget _body() {
+    return SafeArea(
+      child: Column(
+        children: <Widget>[
+          _selector(),
+          _expenses(),
+          _graph(),
+          Container(
+            color: Colors.blueAccent.withOpacity(0.15),
+            height: 20,
+          ),
+          _list(),
+        ],
+      ),
+    );
+  }
+
+  Widget _pageItem(String name, int position) {
+    Alignment alignment;
+    const selected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.bold,
+      color: Colors.blueGrey,
+    );
+    final unselected = TextStyle(
+      fontSize: 20.0,
+      fontWeight: FontWeight.normal,
+      color: Colors.blueGrey.withOpacity(0.4),
+    );
+
+    if (position == currentPage) {
+      alignment = Alignment.center;
+    } else if (position > currentPage) {
+      alignment = Alignment.centerRight;
+    } else {
+      alignment = Alignment.centerLeft;
+    }
+
+    return Align(
+      alignment: alignment,
+      child: Text(name,
+        style: position == currentPage ? selected : unselected,
+      ),
+    );
+  }
+
+  Widget _selector() {
+    return SizedBox.fromSize(
+      size: const Size.fromHeight(70.0),
+      child: PageView(
+        onPageChanged: (newPage){
+          setState(() {
+            currentPage=newPage;
+          });
+        },
+        controller:_controller,
+        children: const <Widget>[
+          Text('Enero'),
+          Text('Febrero'),
+          Text('Marzo'),
+          Text('Abril'),
+          Text('Mayo'),
+          Text('Junio'),
+          Text('Julio'),
+          Text('Agosto'),
+          Text('Setiembre'),
+          Text('Noviembre'),
+          Text('Diciembre')
+        ],
+      ),
+    );
+  }
+
+  Widget _expenses() {
+    return Column(
+      children: const <Widget>[
+        Text(
+          'S/2,000.00',
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: 40.0,
+          ),
+        ),
+        Text(
+          'Total de Gastos',
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16.0,
+              color: Colors.blueGrey),
+        )
+      ],
+    );
+  }
+
+  Widget _graph() {
+    return const SizedBox(height: 250.0, child: LineChartSample2());
+  }
+
+  Widget _itemList(IconData icon, String nombre, int percent, double value) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 32.0,
+      ),
+      title: Text(
+        nombre,
+        style: const TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 20.0,
+        ),
+      ),
+      subtitle: Text(
+        '$percent% del presupuesto',
+        style: const TextStyle(
+          fontSize: 16.0,
+          color: Colors.blueGrey,
+        ),
+      ),
+      trailing: Container(
+          decoration: BoxDecoration(
+            color: Colors.blueAccent.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(
+              'S/.$value',
+              style: const TextStyle(
+                color: Colors.blueAccent,
+                fontWeight: FontWeight.w500,
+                fontSize: 16,
+              ),
+            ),
+          )),
+    );
+  }
+
+  Widget _list() {
+    return Expanded(
+        child: ListView.separated(
+          itemCount: 14,
+          itemBuilder: (BuildContext context, int index) =>
+              _itemList(FontAwesomeIcons.cartShopping, 'Compras', 14, 145.12),
+          separatorBuilder: (BuildContext context, int index) {
+            return Container(
+              color: Colors.blueAccent.withOpacity(0.15),
+              height: 8,
+            );
+          },
+          /*children: const <Widget>[
+      _itemList(FontAwesomeIcons.cartShopping, 'Compras', 14, 145.12),
+      _itemList(FontAwesomeIcons.wineGlass, 'Alcohol', 10, 73.3),
+      _itemList(FontAwesomeIcons.burger, 'Comida', 11, 123.32),
+      _itemList(FontAwesomeIcons.cartShopping, 'Compras', 10, 100.12)
+    ],*/
+        ));
+  }
+}
