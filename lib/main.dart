29c58a725fd18f03e3fff465f6c4d8a720f90da9
Fcampos05 <0512018034@alumnos.unp.edu.pe>
2023-02-01 11:12:40 -0500

    update main

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:gasto_wallet/pages/add_page.dart';
+import 'package:gasto_wallet/pages/home_page.dart';
 import 'month_widget.dart';
 import 'login.dart';
 
@@ -24,162 +24,12 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+
+      routes: {
+        '/': (BuildContext contex) => HomePage(),
+        '/add': (BuildContext contex) => AddPage(),
+      },
     );
   }
 }
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  int currentPage = 9;
-  late PageController _controller;
-  late Stream<QuerySnapshot> _query;
-
-  @override
-  void initState() {
-    super.initState();
-    _query = FirebaseFirestore.instance
-        .collection('expenses')
-        .where('month', isEqualTo: currentPage + 1)
-        .snapshots();
-    _controller = PageController(
-      initialPage: currentPage,
-      viewportFraction: 0.4,
-    );
-  }
-
-  Widget _bottomAction(IconData icon) {
-    return InkWell(
-      child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Icon(icon),
-      ),
-    );
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      bottomNavigationBar: BottomAppBar(
-        notchMargin: 8.0,
-        shape: const CircularNotchedRectangle(),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: <Widget>[
-            _bottomAction(FontAwesomeIcons.clockRotateLeft),
-            _bottomAction(FontAwesomeIcons.chartPie),
-            const SizedBox(width: 48.0),
-            _bottomAction(FontAwesomeIcons.wallet),
-            _bottomAction(Icons.settings),
-          ],
-        ),
-      ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(
-              builder: (BuildContext context) {
-                return const Login();
-              },
-            ),
-          );
-        },
-        child: const Icon(Icons.add),
-      ),
-      body: _body(),
-    );
-  }
-
-  Widget _body() {
-    return SafeArea(
-      child: Column(
-        children: <Widget>[
-          _selector(),
-          StreamBuilder<QuerySnapshot>(
-            stream: _query,
-            builder: (BuildContext contex, AsyncSnapshot<QuerySnapshot> data) {
-              if(data.hasData){
-                return MonthWidget(documents: data.data!.docs);
-              }else{
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }
-            }
-          ),
-
-        ],
-      ),
-    );
-  }
-
-  Widget _pageItem(String name, int position) {
-    Alignment alignment;
-    const selected = TextStyle(
-      fontSize: 20.0,
-      fontWeight: FontWeight.bold,
-      color: Colors.blueGrey,
-    );
-    final unselected = TextStyle(
-      fontSize: 20.0,
-      fontWeight: FontWeight.normal,
-      color: Colors.blueGrey.withOpacity(0.4),
-    );
-
-    if (position == currentPage) {
-      alignment = Alignment.center;
-    } else if (position > currentPage) {
-      alignment = Alignment.centerRight;
-    } else {
-      alignment = Alignment.centerLeft;
-    }
-
-    return Align(
-      alignment: alignment,
-      child: Text(name,
-        style: position == currentPage ? selected : unselected,
-      ),
-    );
-  }
-
-  Widget _selector() {
-    return SizedBox.fromSize(
-      size: const Size.fromHeight(70.0),
-      child: PageView(
-        onPageChanged: (newPage){
-          setState(() {
-            currentPage = newPage;
-            _query = FirebaseFirestore.instance
-                .collection('expenses')
-                .where('month', isEqualTo: currentPage + 1)
-                .snapshots();
-          });
-        },
-        controller:_controller,
-        children: <Widget>[
-          _pageItem("Enero", 0),
-          _pageItem("Febrero", 1),
-          _pageItem("Marzo", 2),
-          _pageItem("Abril", 3),
-          _pageItem("Mayo", 4),
-          _pageItem("Junio", 5),
-          _pageItem("Julio", 6),
-          _pageItem("Agosto", 7),
-          _pageItem("Septiembre", 8),
-          _pageItem("Octubre", 9),
-          _pageItem("Noviembre", 10),
-          _pageItem("Diciembre", 11),
-        ],
-      ),
-    );
-  }
-
-
-}
